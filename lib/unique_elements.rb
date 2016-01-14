869a187adbff6941942e4ee276eb2cb5c4f369e8
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-01-14 16:54:18 -0600

    my solution for the unique elements problem

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,20 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
+  counts = Hash.new(0)
   
+  arr.each do |obj|
+    counts[obj] += 1
+  end
+
+  uniq_arr = []
+    
+  counts.each do |k, v|
+      if v == 1
+          uniq_arr << k 
+      end
+  end
+    
+  return uniq_arr
+
 end
